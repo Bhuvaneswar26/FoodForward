3a0e86fc5bcf0a02939d4f4ea06c6c0647dd3a1b
Vasanth N <vasanthnimmala69@outlook.com>
2024-01-26 14:54:05 +0530

    Project FF

--- db/dbconnection.js
+++ db/dbconnection.js
@@ -1,25 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const connect = async (dburl) =>{
+const connect = async (dburl) => {
+  const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
 
-
-    const options = {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    };
-
-    try{
-
-    await mongoose.connect(dburl,options)
-    console.log("connected")
-
-    }
-    catch (err){
-        console.log("err is err")
-    }
-
-
-
-}
+  try {
+    await mongoose.connect(dburl, options);
+    console.log("connected");
+  } catch (err) {
+    console.log("err is err");
+  }
+};
 
 module.exports = connect;
